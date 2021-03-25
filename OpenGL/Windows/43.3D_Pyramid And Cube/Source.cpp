1c2a1319a65dc54da1bc0d524ab5968cab9b99e5
Darshanvmali <darshanvmali@gmail.com>
2021-03-25 12:07:37 +0530

    Pushing Linux Assignment

--- OpenGL/Windows/43.3D_Pyramid And Cube/Source.cpp
+++ OpenGL/Windows/43.3D_Pyramid And Cube/Source.cpp
@@ -313,35 +313,35 @@ void Display()
     glVertex3f(-0.5f, -0.5f, 0.5f);
     glVertex3f(0.5f, -0.5f, 0.5f);
 
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
+    glColor3f(0.0f, 1.0f, 0.0f   );
+    glVertex3f(0.5f, 0.5f, -0.5f );
+    glVertex3f(0.5f, 0.5f, 0.5f  );
+    glVertex3f(0.5f, -0.5f, 0.5f );
     glVertex3f(0.5f, -0.5f, -0.5f);
 
     glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(0.5f, 0.5f, -0.5f   );
+    glVertex3f(-0.5f, 0.5f, -0.5f  );
+    glVertex3f(-0.5f, -0.5f, -0.5f );
+    glVertex3f(0.5f, -0.5f, -0.5f  );
 
     glColor3f(1.0f, 0.0f, 1.0f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f, 0.5f, -0.5f  );
+    glVertex3f(-0.5f, 0.5f, 0.5f   );
+    glVertex3f(-0.5f, -0.5f, 0.5f  );
+    glVertex3f(-0.5f, -0.5f, -0.5f );
 
-    glColor3f(0.0f, 1.0f, 1.0f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
+    glColor3f(0.0f, 1.0f, 1.0f  );
+    glVertex3f(0.5f, 0.5f, 0.5f     );
+    glVertex3f(-0.5f, 0.5f, 0.5f    );
+    glVertex3f(-0.5f, 0.5f, -0.5f   );
+    glVertex3f(0.5f, 0.5f, -0.5f    );
 
     glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(0.5f, -0.5f, 0.5f    );
+    glVertex3f(-0.5f, -0.5f, 0.5f   );
+    glVertex3f(-0.5f, -0.5f, -0.5f  );
+    glVertex3f(0.5f, -0.5f, -0.5f   );
 
     glEnd();
 
